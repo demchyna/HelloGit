78c7281a63dbf80cd972016664b666ba6dfce2ac
Mykola Demchyna <kdemchyna@gmail.com>
2020-12-21 15:27:02 +0200

    Five times "Hello Git!"

--- src/main/java/com/softserve/itacademy/App.java
+++ src/main/java/com/softserve/itacademy/App.java
@@ -2,6 +2,7 @@ package com.softserve.itacademy;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println("Hello Git!");
+        for(int i=0; i<5; i++)
+            System.out.println("Hello Git!");
     }
 }
